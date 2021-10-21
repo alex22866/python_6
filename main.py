17e77355435558ae1d305b1700cd06bc7cab1a38
alex22866 <isaeva85sasha@gmail.com>
2021-10-21 13:07:42 +0600

    6 lesson

---
+++ main.py
@@ -0,0 +1,52 @@
+# array = [1, 5, 5, 6, 'string', [1, 2], 1.2]
+# array = [228, 77, 4, 'string', 9, [0.8], 3.2
+# array = [1, 2, 3, 4, 'str', [1.2], 1.2]
+# array = [2, 14, 'str', [1.2], 1.2 ]
+# array = [1, 3, 5, 'str', 1, [1.2], 1.2] #remove удаляет число
+# array = [1, 2, 3, 'str', 1, [1.2], 1.2]
+# array = [1, 2, 3, 'str', 1, [1.2], 1.2]
+
+# list = [1, 22, 33, 44, -5, 0]
+# list.append(100)
+# list = [1, 22, 33, 44, -5, 0]
+# list.insert(1000, -3)
+# list = [1, 33, 44, [1.2] ]
+# list.count()
+
+# spisok = []
+# numbers = [3, 4, 5, 8, 10]
+# print(numbers)
+# numbers = [3, 5, 34.1, 'name']
+# print('numbers')
+
+# name = ['sonya', 'gosha', 12]  #доб элемент
+# name.append('gogaa')
+# print(name)
+
+# name.pop() #удаляет последний элемент
+# print(name)
+
+# n = name.index('sonya') #возвращает индекс какого либо элемента из списка
+# print(n)
+
+# name = ['hello', 'gosha', 'world'] #измеряет длинну списка
+# print(len(name))
+
+#letters = [4, 6, 12, 66, 111, 665]  # пишет список в порядке убывания/возрастания
+
+#letters.sort(reverse=True)
+#print(letters)
+
+#name = ['hkjdhg', 'kfhdkf', 6, 4, 9, 1111]  # cтавит вместо цифры букву
+#letters[1] = 'M'
+
+#letters = [55, 77, 99.0, 88, 77.8]
+#letters.sort()  #cортирует списки
+#print(letters)
+
+l = [1, 88, 'c', 2.34, 'gg', ['s', 't', 'r']]
+l.append(228)
+l.append(21)
+b=[11, 5]
+print(l)
+
---
+++ main2.py
@@ -0,0 +1,19 @@
+#name = ['sonya', 'gosha', 12]
+#name.append('gogaa')
+#print(name)
+
+#name.pop()
+#print(name)
+
+#n = name.index('sonya')
+#print(n)
+
+#name = ['hello', 'gosha', 'world']
+#print(len(name))
+
+name = [4, 6, 12, 66, 111, 665]
+name.sort(reverse=False)
+print(name)
+
+name = [8, 5, 3, 90]
+name.reverse(name)
